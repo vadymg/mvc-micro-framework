46d9f5c7b99de7f3d879ddcf1d865dcd9316188a
vadymg <vadym.gerega@gmail.com>
2020-10-19 17:19:20 +0300

    added phpunit

---
+++ tests/ControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use VG\core\Controller;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ControllerTest
+ */
+class ControllerTest extends TestCase
+{
+
+    public function testRender()
+    {
+        $this->assertIsBool(true, "This is not true");
+    }
+}
